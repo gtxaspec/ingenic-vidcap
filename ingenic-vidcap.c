57bcdafa18b85322af9f77f11207c9b6fd853532
gtxaspec <gtxent@gmail.com>
2023-08-28 02:56:00 -0700

    change to tcp

--- ingenic-vidcap.c
+++ ingenic-vidcap.c
@@ -5,12 +5,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
- 
+#include <arpa/inet.h>
+#include <netinet/tcp.h>
+
 #include "capture_and_encoding.h"
 #include "version.h"
 
-char const* inputVideoFileName = "/tmp/h264_fifo";
-char const* inputAudioFileName = "/tmp/g711_fifo";
+#define SERVER_PORT 8080
+#define BUFFER_SIZE 4096  // 4 KB
 
 void displayUsage() {
     printf("usage: ingenic-vidcap [args...]\n\n"
@@ -25,8 +27,8 @@ int main(int argc, char** argv) {
     printf("ingenic-vidcap version: %s\n", VERSION);
 
     // parse args
-     int i;
-      for (i = 0; i < argc; i++) {
+    int i;
+    for (i = 0; i < argc; i++) {
         char *arg = argv[i];
 
         if (*arg == '-') {
@@ -55,21 +57,60 @@ int main(int argc, char** argv) {
         printf("unable to setup camera stream\n");
         exit(1);
     }
-    unlink(inputVideoFileName);
 
-    if (mkfifo(inputVideoFileName, 0777) < 0) {
-        printf("mkfifo Failed\n");
+    // Setup TCP server
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Socket creation failed");
         exit(1);
     }
 
-    int fd = open(inputVideoFileName, O_RDWR | O_CREAT | O_TRUNC, 0777);
-    if (fd < 0) {
-        printf("Failed open fifo\n");
+    int opt_val = 1;
+    setsockopt(server_socket, IPPROTO_TCP, TCP_NODELAY, &opt_val, sizeof(opt_val));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Bind failed");
+        close(server_socket);
         exit(1);
     }
+
+    if (listen(server_socket, 1) == -1) {
+        perror("Listen failed");
+        close(server_socket);
+        exit(1);
+    }
+
     while (1) {
-        if (get_stream(fd ,0) < 0) break; // Get stream and write to fifo.
+        printf("Waiting for client connection...\n");
+        client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_len);
+        if (client_socket == -1) {
+            perror("Accept failed");
+            continue; // Go back to waiting for another client if accept() failed
+        }
+        printf("Client connected.\n");
+
+        // Reduce buffering for the accepted client socket
+        setsockopt(client_socket, IPPROTO_TCP, TCP_NODELAY, &opt_val, sizeof(opt_val));
+        int bufferSize = BUFFER_SIZE;
+        setsockopt(client_socket, SOL_SOCKET, SO_SNDBUF, &bufferSize, sizeof(bufferSize));
+
+        while (1) {
+            if (get_stream(client_socket, 0) < 0) break; // Get stream and send to client.
+        }
+
+        close(client_socket);
+        printf("Client disconnected.\n");
     }
 
-    return 0; // only to prevent compiler warning
+    close(server_socket); // This might never be reached due to the infinite loop, but it's good practice to have it.
+
+    return 0;
 }
